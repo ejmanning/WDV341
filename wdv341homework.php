73bddfbf000c0bdeb363f8a80f2c278b3021f6b6
Erica Manning <60369190+ejmanning@users.noreply.github.com>
2020-09-10 15:39:38 -0500

    Create wdv341homework.php

---
+++ wdv341homework.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>WDV 341 Intro to PHP</title>
+  <!-- Author: Erica Manning
+  Date: August 27, 2020-->
+<style>
+div#container	{
+  	width:60%;
+  	margin:auto;
+  	padding:10px;
+  	background-color:#ffbde0;
+    background-image: linear-gradient(#e35fa1, pink);;
+  	border:thin solid black;
+    border-radius: 100px;
+    box-shadow: 10px 10px #705262;
+  }
+
+h1	{
+    text-decoration:underline;
+}
+
+h3:hover {
+  background-color: #edd5b2;
+}
+h3 {
+  background-color: white;
+  width: 50%;
+  margin: auto;
+  margin-top: 2%;
+  padding: 0.5%;
+  border-left: thick solid black;
+  border-top-right-radius: 30px;
+  border-bottom-right-radius: 30px;
+}
+
+body	{
+  text-align: center;
+  background-color: #abc7ff;
+	background-image: linear-gradient(to right, #abc7ff , #ccfcb6);;
+}
+
+</style>
+</head>
+  <body>
+    <div id="container">
+      <h1>WDV 341 Intro to PHP</h1>
+      <h2><a href="http://ericamanning.com">Erica Manning</a></h2>
+    </div>
+
+    <h3><a href="3-1PHPBasics.php">3-1 PHP Basics</a></h3>
+    <h3><a href="4-1PHPFunctions.php">4-1 PHP Functions</a></h3>
+
+
+
+  </body>
+</html>
