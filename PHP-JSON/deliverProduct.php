a1320507091ab912d213cd11c56720195e6b789a
Erica Manning <60369190+ejmanning@users.noreply.github.com>
2020-10-13 15:06:35 -0500

    Create deliverProduct.php

---
+++ PHP-JSON/deliverProduct.php
@@ -0,0 +1,15 @@
+<?php
+
+	$productObj = new stdClass();
+
+	$productObj->productName = "PHP Textbook";
+	$productObj->productPrice = "$129.95";
+	$productObj->productPageCount = 327;
+	$productObj->productISBN = "13-1234435690";
+
+	echo $productObj;
+//
+	$returnObj = json_encode($productObj);	//create the JSON object
+//
+	echo $returnObj;							//send results back to calling program
+?>
